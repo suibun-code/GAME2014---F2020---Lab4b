1994016864f3ca659028963c19bc96c776f987ec
Ramin Amiri <zeuspsn.55555@gmail.com>
2020-10-17 02:19:55 -0400

    Enemy animation and movement

---
+++ Assets/_Scripts/EnemyController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    public float horizontalSpeed;
+    public float horizontalBoundary;
+    public float direction;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        CheckBounds();
+    }
+
+    private void Move()
+    {
+        transform.position += new Vector3(horizontalSpeed * direction * Time.deltaTime, 0.0f, 0.0f);
+    }
+
+    private void CheckBounds()
+    {
+        //Checking right boundary.
+        if (transform.position.x >= horizontalBoundary)
+            direction = -1.0f;
+
+         if (transform.position.x <= -horizontalBoundary)
+            direction = 1.0f;
+
+        //Checking left boundary.
+    }
+}
