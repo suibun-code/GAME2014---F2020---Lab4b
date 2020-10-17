604d4baf0c385861bab72aa85e361f636e41faef
Ramin Amiri <zeuspsn.55555@gmail.com>
2020-10-17 02:58:23 -0400

    progress on bullet factory

---
+++ Assets/_Scripts/BulletFactory.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class BulletFactory : MonoBehaviour
+{
+    [Header("Bullet Types")]
+    public GameObject normalBullet;
+    public GameObject fatBullet;
+    public GameObject pulsatingBullet;
+
+    public GameObject createBullet(BulletType type)
+    {
+        GameObject tempBullet = null;
+
+        switch (type)
+        {
+            case BulletType.NORMAL:
+                tempBullet = Instantiate(normalBullet);
+                break;
+
+            case BulletType.FAT:
+                tempBullet = Instantiate(fatBullet);
+                break;
+
+            case BulletType.PULSATING:
+                tempBullet = Instantiate(pulsatingBullet);
+                break;
+        }
+
+        tempBullet.transform.parent = transform;
+        tempBullet.SetActive(false);
+
+        return tempBullet;
+    }
+}
--- Assets/_Scripts/BulletManager.cs
+++ Assets/_Scripts/BulletManager.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [System.Serializable]
 public class BulletManager : MonoBehaviour
 {
-    public GameObject bullet;
+    public BulletFactory bulletFactory;
     public int MaxBullets;
 
     private Queue<GameObject> m_bulletPool;
@@ -23,9 +23,7 @@ public class BulletManager : MonoBehaviour
 
         for (int count = 0; count < MaxBullets; count++)
         {
-            var tempBullet = Instantiate(bullet);
-            tempBullet.SetActive(false);
-            tempBullet.transform.parent = transform;
+            var tempBullet = bulletFactory.createBullet(BulletType.NORMAL);
             m_bulletPool.Enqueue(tempBullet);
         }
     }
